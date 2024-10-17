14c46f96a88c6fff37385476a3cb62dddba0401b
mrsi13nt <androgeme0@gmail.com>
2024-10-17 14:04:55 -0400

    some updates

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ def linux():
         if os.path.exists(destination_directory):
             print(f"Removing existing directory: {destination_directory}")
             shutil.rmtree(destination_directory)
-        subprocess.run('pip install -r requirements.txt --break-system-packages',shell=True) # requirments
+        subprocess.run('pip3 install -r requirements.txt --break-system-packages',shell=True) # requirments
         subprocess.run(f'mkdir -p {home_directory}/.VulneraX',shell=True)
         print(f"Copying files from {current_directory} to {destination_directory}...")
         # Copy files from current directory to the destination (this includes .git)
